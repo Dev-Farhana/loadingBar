e88cfb5e1c2c2db3e4166061391c6ed46b6e93fc
Farhana <124885288+Dev-Farhana@users.noreply.github.com>
2023-08-21 13:05:44 +0500

    finish.

---
+++ app.js
@@ -0,0 +1,14 @@
+        const counterEle = document.querySelector('.counter');
+        const barEle = document.querySelector('.loading-bar-front');
+        let idx = 0;
+
+
+function updateNum(){
+    counterEle.innerText = idx + "%";
+    barEle.style.width = idx + "%";
+    idx++;
+    if(idx < 101){
+        setTimeout(updateNum, 20);
+    }
+}
+updateNum()
